7f5d5ca890f4821c25f5564e74ed793e8931a6b8
Natalia Alpízar <natalpizarm@gmail.com>
2025-02-09 14:01:05 +0100

    refactor: change structure from Create React App to use Vite (#2)

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="about-section">
+          <div className="profile-image">
+              <h1>Hi, I'm Natalia<span className='big'>.</span></h1>
+        
+              <h2>I'm a <span>Frontend Engineer</span></h2>
+
+              <p>
+                With 10 years of experience in creating user-centric, responsive,
+                and visually engaging interfaces. I specialize in building intuitive websites and
+                applications that prioritize performance, accessibility, and responsiveness. Currently based in Berlin.
+              </p>
+          </div>
+      </section>
+      <footer className="footer-section">
+        <p>© 2025 Natalia Alpizar | <a href="mailto:natalpizarm@gmail.com">natalpizarm@gmail.com</a></p>
+      </footer>
+    </>
+  )
+}
+
+export default App
