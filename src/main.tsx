b785403d039e587368b35e05332988e2eb216543
nalpizar <natalpizarm@gmail.com>
2025-02-16 21:24:16 +0100

    feat: add readme and some refactor for CSS

--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,25 @@ createRoot(document.getElementById('root')!).render(
       <Navigation />
       <App />
     </div>
+    <footer className="footer">
+      <p className="footer__text">
+        &copy; 2025 Natalia Alpizar Meza. All rights reserved.
+      </p>
+      <p className="footer__text">
+        Let's connect! Reach out via{' '}
+        <a className="footer__text-link" href="mailto:natalpizarm@gmail.com">
+          email
+        </a>
+        , or{' '}
+        <a
+          className="footer__text-link"
+          href="https://www.linkedin.com/in/nataliaalpizar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
+    </footer>
   </StrictMode>
 );
