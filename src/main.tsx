6bdaaaed1f85fa967eb0b4ac6627a5cd21733d25
nalpizar <natalpizarm@gmail.com>
2025-02-17 15:23:12 +0100

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,33 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="home">
-      <Navigation />
-      <App />
-    </div>
-    <footer className="footer">
-      <p className="footer__text">
-        &copy; 2025 Natalia Alpizar Meza. All rights reserved.
-      </p>
-      <p className="footer__text">
-        Let's connect! Reach out via{' '}
-        <a className="footer__text-link" href="mailto:natalpizarm@gmail.com">
-          email
-        </a>
-        , or{' '}
-        <a
-          className="footer__text-link"
-          href="https://www.linkedin.com/in/nataliaalpizar"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </p>
-    </footer>
+    <BrowserRouter>
+      <div className="home">
+        <Navigation />
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   </StrictMode>
 );
